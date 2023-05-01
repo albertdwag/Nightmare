d00050308742ef78d777142de1b923120e1cdc20
albertdwag <albertcesar463@gmail.com>
2023-05-01 17:53:28 -0300

    adding the inputmanager and bindings.
    adding the input manager with the bindings to the movement of the player and the movement for the camera.

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
+    public static InputManager Instance
+    {
+        get { return _instance; }
+    }
+
     private PlayerControls playerControls;
+    private static InputManager _instance;
 
     private void Awake()
     {
+        if (_instance != null && _instance != this)
+            Destroy(this);
+        else
+            _instance = this;
+
         playerControls = new PlayerControls();
     }
 
@@ -24,7 +35,7 @@ public class InputManager : MonoBehaviour
     {
         return playerControls.Player.Movement.ReadValue<Vector2>();
     }
-    public Vector2 GetMouseDel()
+    public Vector2 GetMouseDelta()
     {
         return playerControls.Player.Look.ReadValue<Vector2>();
     }
