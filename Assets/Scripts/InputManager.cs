738beaf5814d3b4095942953f576190878f89ed9
albertdwag <albertcesar463@gmail.com>
2023-05-01 15:56:22 -0300

    Initial commit

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private PlayerControls playerControls;
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
+    public Vector2 GetPlayerMovement()
+    {
+        return playerControls.Player.Movement.ReadValue<Vector2>();
+    }
+    public Vector2 GetMouseDel()
+    {
+        return playerControls.Player.Look.ReadValue<Vector2>();
+    }
+}
