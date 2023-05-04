e96863cdc65c751df44d567a0639d357a957ed7c
albertdwag <albertcesar463@gmail.com>
2023-05-04 11:08:42 -0300

    updating the ammo ui.
    using scritables objects to update the ui and adding an icon to represent the ammo hud.

--- Assets/Scripts/Player/GunBaseController.cs
+++ Assets/Scripts/Player/GunBaseController.cs
@@ -4,29 +4,38 @@ using UnityEngine;
 
 public class GunBaseController : MonoBehaviour
 {
-    public float range = 100f;
+    [SerializeField] private SOGunSetup _gunSetup;
+    [SerializeField] private UiUpdater _uiUpdater;
 
-    private int _maxAmmo = 7;
-    [SerializeField] private int _currentAmmo;
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && _gunSetup._currentAmmo != 0)
+        {
             Shoot();
+            _gunSetup._currentAmmo--;
+            UpdateAmmo();
+        }
     }
 
     private void Start()
     {
-        _currentAmmo = _maxAmmo;
+        _gunSetup._currentAmmo = _gunSetup._maxAmmo;
     }
 
     public void Shoot()
     {
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit, range))
+        if (Physics.Raycast(transform.position, transform.forward, out hit, _gunSetup.range))
         {
             Debug.Log("Hit object: " + hit.transform.name);
         }
-        Debug.DrawRay(transform.position, transform.forward * range, Color.green, 0.1f);
+        Debug.DrawRay(transform.position, transform.forward * _gunSetup.range, Color.green, 0.1f);
+    }
+
+    private void UpdateAmmo()
+    {
+        if (_uiUpdater != null)
+            _uiUpdater.UpdateValue(_gunSetup._currentAmmo);
     }
 }
---
+++ Assets/Scripts/Scritable Objects Scripts/SOGunSetup.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+public class SOGunSetup : ScriptableObject
+{
+    public float range = 100f;
+    public int _maxAmmo = 7;
+    public int _currentAmmo;
+}
